46f737d592791a722f7ae19a21130585ab821cff
Jimmy <toluadejimi@gmail.com>
2024-06-06 05:44:34 +0100

    remove fund added

--- core/resources/views/templates/basic/layouts/main2.blade.php
+++ core/resources/views/templates/basic/layouts/main2.blade.php
@@ -263,7 +263,7 @@
 <footer class="pc-footer">
     <div class="footer-wrapper container-fluid">
         <div class="text-center">
-            <h6 class="text-muted">2024</h6>|<h6>FADDED LOGS STORE</h6>
+            <h6 class="">2024 | FADDED LOGS STORE</h6>
         </div>
     </div>
 </footer><!-- Required Js -->
