db35aaf5a72ef40c8eb51aecd19e6b661b3d6a57
Jimmy <toluadejimi@gmail.com>
2024-06-19 03:24:59 +0100

    remove fund added

--- core/app/Http/Controllers/User/ProfileController.php
+++ core/app/Http/Controllers/User/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
