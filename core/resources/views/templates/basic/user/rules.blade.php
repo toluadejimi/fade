8f88a4fd7acdde973acd663a9554602f0aab003f
Jimmy <toluadejimi@gmail.com>
2024-06-24 21:29:18 +0100

    remove fund added

--- core/resources/views/templates/basic/user/rules.blade.php
+++ core/resources/views/templates/basic/user/rules.blade.php
@@ -1,63 +1,68 @@
 @extends($activeTemplate.'layouts.main2')
 @section('content')
 
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card-body p-4">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex flex-wrap  px-0">
-                        <div class="row mt-12">
+    <div class="pc-container">
+        <div class="pc-content">
+
+            <div class="col-md-6">
+                <div class="card-body p-4">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex flex-wrap  px-0">
+                            <div class="row mt-5">
 
 
-                            <div class="card">
-                                <div class="card-body">
+                                <div class="card">
+                                    <div class="card-body">
 
-                                    <h5> RULES AT FADDED SOCIAL</h5>
+                                        <h5> RULES AT FADDED SOCIAL</h5>
 
-                                    <ul>
+                                        <ul>
 
-                                        <li>Always Secure your accts few hours after login
-                                        </li>
+                                            <li>Always Secure your accts few hours after login
+                                            </li>
 
-                                        <hr>
+                                            <hr>
 
-                                        <li>Accounts cannot be replaced after changing the password.</li>
+                                            <li>Accounts cannot be replaced after changing the password.</li>
 
-                                        <hr>
-                                        <li>We replace bad accounts, if fault is from us (not after use)</li>
-                                        <hr>
-                                        <li>This rules can be changed at any time without prior notice.</li>
-                                        <hr>
-                                        <li>Obscene language to the admins may be grounds for service refusal.</li>
-                                        <hr>
-                                        <li>Ignorance of the rules does not absolve you of responsibility.</li>
-                                        <hr>
-                                        <li>The response time for technical support and the resolution of all
-                                            problems/claims is 24/7.
-                                        </li>
-                                        <hr>
-                                        <li>Accounts are always checked by our private program on private mobile proxy
-                                            prior to sale, so we can guarantee 100% validity of the items.
-                                        </li>
-                                        <hr>
-                                        <li>Accounts cannot be returned; instead, they can only be replaced if bad,
-                                            provided that other rules are complied with.
-                                        </li>
-                                        <hr>
-                                        <li>The store is not liable for any account activity. How your account will last
-                                            depends on how it’s used. No replacement or refund for an account
-                                            suspended/disabled/logged out after a successful login.
-                                        </li>
+                                            <hr>
+                                            <li>We replace bad accounts, if fault is from us (not after use)</li>
+                                            <hr>
+                                            <li>This rules can be changed at any time without prior notice.</li>
+                                            <hr>
+                                            <li>Obscene language to the admins may be grounds for service refusal.</li>
+                                            <hr>
+                                            <li>Ignorance of the rules does not absolve you of responsibility.</li>
+                                            <hr>
+                                            <li>The response time for technical support and the resolution of all
+                                                problems/claims is 24/7.
+                                            </li>
+                                            <hr>
+                                            <li>Accounts are always checked by our private program on private mobile
+                                                proxy
+                                                prior to sale, so we can guarantee 100% validity of the items.
+                                            </li>
+                                            <hr>
+                                            <li>Accounts cannot be returned; instead, they can only be replaced if bad,
+                                                provided that other rules are complied with.
+                                            </li>
+                                            <hr>
+                                            <li>The store is not liable for any account activity. How your account will
+                                                last
+                                                depends on how it’s used. No replacement or refund for an account
+                                                suspended/disabled/logged out after a successful login.
+                                            </li>
 
-                                    </ul>
+                                        </ul>
 
+
+                                    </div>
 
                                 </div>
-
                             </div>
-                        </div>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
